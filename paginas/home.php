13ba6c390ff9d00cbcc7050927ecab1f32e54991
WayDiegonis <diegoneveslourenco@gmail.com>
2019-10-31 20:36:36 -0300

    mod

--- paginas/home.php
+++ paginas/home.php
@@ -5,26 +5,30 @@
 	<div class="container">
 	  <div class="row">
 		    <div class="col-sm-4">
-		      <div class="card" style="width: 18rem;">
-			  <img class="card-img-top" src="http://www.ikis.com.br/wp-content/uploads/2018/01/como-faze-um-site-profissional-2.jpg" alt="Imagem do Site" height="215">
-			  <div class="card-body">
-			    <h5 class="card-title">Sobre o Site</h5>
-			    <p class="card-text">Você encontrará informações de quem criou o site, os motivos e outras coisas</p>
-			    <a href="?pg=sobre" class="btn btn-primary">Sobre o Site</a>
-			  </div>
-			</div>
-		    </div>
+		    	<div>
+		    		<div class="card" style="width: 18rem;">
+			  			<img class="card-img-top" src="http://www.ikis.com.br/wp-content/uploads/2018/01/como-faze-um-site-profissional-2.jpg" alt="Imagem do Site" height="215">
+			  			<div class="card-body">
+			    			<h5 class="card-title">Sobre o Site</h5>
+			    			<p class="card-text">Você encontrará informações de quem criou o site, os motivos e outras coisas</p>
+			    			<a href="?pg=sobre" class="btn btn-primary">Sobre o Site</a>
+			  			</div>
+					</div>
 
-		    <div class="col-sm-4">
-		      <div class="card" style="width: 18rem;">
-			  <img class="card-img-top" src="https://img.ibxk.com.br/2016/11/30/30161442629415.jpg" alt="Imagem de LoL e CS" height="215">
-			  <div class="card-body">
-			    <h5 class="card-title">Informações dos Jogos</h5>
-			    <p class="card-text">Você encontrará informações sobre os jogos selecionados para o projeto.</p>
-			    <a href="?pg=sobrejogos" class="btn btn-primary">Sobre os Jogos</a>
-			  </div>
-			</div>
+
+		      		<div class="card" style="width: 18rem;">
+			  			<img class="card-img-top" src="https://img.ibxk.com.br/2016/11/30/30161442629415.jpg" alt="Imagem de LoL e CS" height="215">
+			  			<div class="card-body">
+			    			<h5 class="card-title">Informações dos Jogos</h5>
+			    			<p class="card-text">Você encontrará informações sobre os jogos selecionados para o projeto.</p>
+			    			<a href="?pg=sobrejogos" class="btn btn-primary">Sobre os Jogos</a>
+			  			</div>
+					</div>
+		    	</div>
 		    </div>
+		      
+
+		    
 
 		    <div class="col-sm-4">
 		      <div class="card" style="width: 18rem;">
